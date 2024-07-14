38d2d8579946224fe297809e62e140dbae88a0f2
CodeWithSuleman <codewithsuleman@gmail.com>
2024-07-14 09:31:54 -0700

    - Edit Typography Class _ Edit AppColor Class

--- lib/constants/app_color.dart
+++ lib/constants/app_color.dart
@@ -3,4 +3,10 @@ import 'package:flutter/material.dart';
 class AppColor {
   static const Color blackColor = Color(0xff000000);
   static const Color whiteColor = Color(0xffFFFFFF);
+  static const Color primaryColor = Color(0xff0D6EFD);
+  static const Color SecondaryColor = Color(0xff1483C2);
+  static const Color dimGrayColor = Color(0xff6A6A6A);
+  static const Color grayishBlueColor = Color(0xff707B81);
+  static const Color darkGrayColor = Color(0xff2B2B2B);
+  static const Color tomatoColor = Color(0xffFf4c24);
 }
--- lib/constants/typography.dart
+++ lib/constants/typography.dart
@@ -5,15 +5,29 @@ class AppTypography {
   static const _raleway = 'Raleway';
   static const _popoins = 'Popins';
 
-  static const TextStyle ralewayheadingLarge = TextStyle(
+  static const TextStyle ralewayMediumHeadingLarge = TextStyle(
       fontSize: 32,
       fontWeight: FontWeight.bold,
       color: AppColor.blackColor,
       letterSpacing: 1.2,
       fontFamily: _raleway,
       height: 1.5);
+  static const TextStyle ralewayHeadingLarge = TextStyle(
+      fontSize: 34,
+      fontWeight: FontWeight.bold,
+      color: AppColor.blackColor,
+      letterSpacing: 1.2,
+      fontFamily: _raleway,
+      height: 1.5);
   static const TextStyle ralewayheadingSemiLarge = TextStyle(
-      fontSize: 24,
+      fontSize: 26,
+      fontWeight: FontWeight.w600,
+      color: AppColor.blackColor,
+      letterSpacing: 1.2,
+      fontFamily: _raleway,
+      height: 1.5);
+  static const TextStyle ralewayMediumHeadingSemiLarge = TextStyle(
+      fontSize: 20,
       fontWeight: FontWeight.w600,
       color: AppColor.blackColor,
       letterSpacing: 1.2,
@@ -21,6 +35,13 @@ class AppTypography {
       height: 1.5);
   static const TextStyle popinsParagraphRegular = TextStyle(
       fontSize: 16,
+      fontWeight: FontWeight.w600,
+      color: AppColor.blackColor,
+      letterSpacing: 1.2,
+      fontFamily: _popoins,
+      height: 1.5);
+  static const TextStyle popinsMediumParagraphRegular = TextStyle(
+      fontSize: 12,
       fontWeight: FontWeight.w600,
       color: AppColor.blackColor,
       letterSpacing: 1.2,
